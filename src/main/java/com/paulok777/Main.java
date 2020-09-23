ee013cbfcc488616e903fa80b9c82fb1fa24dcd5
PauLOk777 <trocjuk2000@gmail.com>
2020-09-23 14:00:50 +0300

    Removed some config files.

--- src/main/java/com/paulok777/Main.java
+++ src/main/java/com/paulok777/Main.java
@@ -6,9 +6,8 @@ import com.paulok777.reader.Reader;
 import java.util.List;
 
 public class Main {
-    @SuppressWarnings("all")
     public static void main(String[] args) {
-        args = new String[] {"example.stk", ""};
+//        args = new String[] {"example.stk", ""};
         if (args.length != 2) throw new IllegalArgumentException();
         if (args[0] == null || args[1] == null) throw new IllegalArgumentException();
         List<String> intermediateCode = Reader.read(args[0], args[1]);
